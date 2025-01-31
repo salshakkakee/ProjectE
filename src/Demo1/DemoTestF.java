57b278970be72d6f3613569bb3de2b627ce43fc7
salsh <salsh@Sally.lan>
2025-01-31 16:15:31 -0500

    new method

---
+++ src/Demo1/DemoTestF.java
@@ -0,0 +1,20 @@
+package Demo1;
+
+public class DemoTestF {
+	public static void main(String[] args) {
+
+		String name[] = new String[5];
+
+		name[0] = "Sally";
+		name[3] = "Zaid";
+		name[1] = "Alma";
+		name[2] = "Tokyo";
+
+		System.out.println(name[1]);
+		System.out.println(name[2]);
+		System.out.println(name[0]);
+		System.out.println(name[3]);
+
+	}
+
+}
